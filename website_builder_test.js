0e655aaf459951910cb05744792b5516cc51d7dc
mikw <you@example.com>
2024-02-14 01:07:49 +0300

    TA-1

--- website_builder_test.js
+++ website_builder_test.js
@@ -1,13 +1,24 @@
 Feature('website_builder');
 
-Scenario('runity test assignment', async ({ I }) => {
-    I.amOnPage('https://www.reg.ru/web-sites/website-builder/');
-    let cost_of_builder = await I.grabTextFrom('.b-plans-pillar__item_type_infinite.i-analytics.i-analytics_event_google.i-analytics_event_yandex .b-money-amount');
-    I.click('.b-plans-pillar__item_type_infinite.i-analytics.i-analytics_event_google.i-analytics_event_yandex .b-plans-pillar__details-indent.l-align_center');
-    I.fillField('.choose-ssl-domain__input-wrap', 'foobar412325.ru');
-    I.click('.choice-list .choice-item:first-child');
-    let cost_of_domain = await I.grabTextFrom('.domain-in-order-price__final');
-    let cost_total = await I.grabTextFrom('.order-footer-price__price')
-    assert.equal(cost_of_builder + cost_of_domain, cost_total)
+start_page = 'https://www.reg.ru/web-sites/website-builder/';
+selector_cost_infin_builder = '.b-plans-pillar__item_type_infinite.i-analytics.i-analytics_event_google.i-analytics_event_yandex .b-money-amount';
+selector_order_button = '.b-plans-pillar__item_type_infinite.i-analytics.i-analytics_event_google.i-analytics_event_yandex .b-plans-pillar__details-indent.l-align_center';
+selector_adding_domain_button = '.ds-textfield.ds-input.ds-textfield_size_m.ds-textfield_state_defaul';
+selector_buying_domain_button = '.choice-list .choice-item:first-child .domain-select-item';
+selector_buying_to_desapear = '.choice-list .choice-item:first-child .domain-select-item .ds-loader__loader.ds-loader__loader_color_default';
+selector_cost_of_ru_zone_domain = '.domain-in-order-price__final';
+selector_cost_total = '.order-footer-price__price';
 
+const assert = require('assert');
+Scenario('runity test assignment', async ({ I }) => {
+    I.amOnPage(start_page);
+    let cost_of_builder = await I.grabTextFrom(selector_cost_infin_builder);
+    I.click(selector_order_button);
+    I.fillField(selector_adding_domain_button, 'foobar412325.ru');
+    I.waitForInvisible(selector_buying_to_desapear);
+    I.click(selector_buying_domain_button);
+    let cost_of_domain = await I.grabTextFrom(selector_cost_of_ru_zone_domain);
+    let cost_total = await I.grabTextFrom(selector_cost_total);
+    assert.equal(Number(cost_of_builder) + Number(cost_of_domain), Number(cost_total));
 });
+
